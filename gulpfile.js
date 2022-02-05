bd41c05cdfb069123ecd8acc009dc57731336e0e
LanaConnor <v.e.d.makc12@gmail.com>
2022-02-05 20:18:09 +0500

    added header

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ const gulp = require('gulp'),
 	cssmin = require('gulp-cssmin');
 
 gulp.task('sass', function () {
-	return gulp.src('app/scss/style.scss')
+	return gulp.src('app/scss/**/*.scss')
 		.pipe(sass({ outputStyle: 'compressed' }))
 		.pipe(rename({ suffix: '.min' }))
 		.pipe(autoprefixer({overrideBrowserslist: ['last 8 versions']}))
@@ -45,7 +45,7 @@ gulp.task('js', function () {
 });
 
 gulp.task('watch', function () {
-	gulp.watch('app/scss/style.scss', gulp.parallel('sass'))
+	gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'))
 	gulp.watch('app/*.html', gulp.parallel('html'))
 	gulp.watch('app/js/*.js', gulp.parallel('js'))
 });
